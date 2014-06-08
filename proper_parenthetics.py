f9ad7fbb439d93cace4bc3130cda92dccf88f835
Perry Hook <perry.hook@gmail.com>
2014-06-08 01:02:10 -0700

    Added new version of proper_parenthetics.py. Now handles truly broken parentheses properly through stack implementation.

--- proper_parenthetics.py
+++ proper_parenthetics.py
@@ -1,3 +1,36 @@
+from stack import Stack
+from stack import EmptyStackError
+
+
+def check_parens(str):
+    """
+    check_parens takes a string and:
+    returns 0 if the number of parentheses is balanced and matched.
+    returns 1 if more left parentheses than right.
+    returns -1 if string has broken (unmatched) parentheses.
+    """
+
+    stack = Stack()
+    for i in range(len(str)):
+        # if loop encounters (, location is pushed to stack.
+        if str[i] == '(':
+            stack.push(i)
+        # else if loop encounters ), attempt to pop from stack. If stack is
+        # empty, catch EmptyStackError and return -1. We have seen too many ).
+        elif str[i] == ')':
+            try:
+                stack.pop()
+            except EmptyStackError:
+                return -1
+    # after going through str looking for parentheses, if there is anything
+    # left on the stack, we have too many (, but if we catch an EmptyStackError
+    # we know the parentheses were balanced.
+    try:
+        stack.pop()
+        return 1
+    except EmptyStackError:
+        return 0
+'''
 def check_parens(str):
     """
     This function meets the requirements given, but it is clearly inadequate
@@ -10,3 +43,4 @@ def check_parens(str):
         return 0
     else:
         return -1
+'''
