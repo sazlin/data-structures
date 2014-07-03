794ca24013dd39b2e48bb59c3fe794eeeed55af9
Perry Hook <perry.hook@gmail.com>
2014-07-02 17:38:44 -0700

    implemented binary search tree with tests and main demonstraing best and worst case runs

---
+++ bst.py
@@ -0,0 +1,120 @@
+import timeit
+from functools import total_ordering
+
+
+@total_ordering
+class BSTNode(object):
+
+    def __init__(self, value, parent=None, left_child=None, right_child=None):
+        self.value = value
+        self.parent = parent
+        self.left = left_child
+        self.right = right_child
+
+    def __lt__(self, other):
+        return self._value < other.get_value()
+
+    def __eq__(self, other):
+        return self._value == other.get_value()
+
+    def is_root(self):
+        return self.parent is None
+
+    def is_leaf(self):
+        return self.left is None and self.right is None
+
+
+class BinarySearchTree(object):
+
+    def __init__(self, values=None):
+        self._size = 0
+        self.root = None
+        if values:
+            [self.insert(val) for val in values]
+
+    def size(self):
+        return self._size
+
+    def insert(self, val):
+        if self.root is None:
+            self.root = BSTNode(val)
+            self._size += 1
+            return
+        current_node = self.root
+        while True:
+            if current_node.value > val:
+                if current_node.left is not None:
+                    current_node = current_node.left
+                else:
+                    current_node.left = BSTNode(val)
+                    self._size += 1
+                    break
+            elif current_node.value < val:
+                if current_node.right is not None:
+                    current_node = current_node.right
+                else:
+                    current_node.right = BSTNode(val)
+                    self._size += 1
+                    break
+            else:
+                # val is already in BST
+                break
+
+    def contains(self, val):
+        if self.root is None:
+            return False
+        current_node = self.root
+        while True:
+            if current_node.value > val:
+                if current_node.left is not None:
+                    current_node = current_node.left
+                else:
+                    return False
+            elif current_node.value < val:
+                if current_node.right is not None:
+                    current_node = current_node.right
+                else:
+                    return False
+            else:
+                return True
+
+    def depth(self):
+        if self.root is None:
+            return 0
+        return self._depth(1, self.root)
+
+    def _depth(self, current_depth, local_root):
+        left_depth = right_depth = 0
+        if local_root.left:
+            left_depth = self._depth(current_depth+1, local_root.left)
+        if local_root.right:
+            right_depth = self._depth(current_depth+1, local_root.right)
+        return max(current_depth, left_depth, right_depth)
+
+    def balance(self):
+        ret_val = 0
+        if self.root is None:
+            return ret_val
+        if self.root.left:
+            ret_val += self._depth(1, self.root.left)
+        if self.root.right:
+            ret_val -= self._depth(1, self.root.right)
+        return ret_val
+
+
+if __name__ == '__main__':
+
+    test_runs = 100000
+
+    balanced_time = timeit.timeit('''
+    balanced_contents = [50, 40, 60, 35, 45, 55, 65, 31, 39, 41, 49, 51, 59, 61, 69]
+    BinarySearchTree(balanced_contents).contains(69)
+    ''', setup="from __main__ import BinarySearchTree", number=test_runs)
+
+    unbalanced_time = timeit.timeit('''
+    unbalanced_contents = [31, 35, 39, 40, 41, 45, 49, 50, 51, 55, 59, 60, 61, 65, 69]
+    BinarySearchTree(unbalanced_contents).contains(69)
+    ''', setup="from __main__ import BinarySearchTree", number=test_runs)
+
+    print "Time for a balanced tree (best case) is: {}\n".format(balanced_time)
+    print "Time for an unbalanced tree (worst case) is: {}\n".format(unbalanced_time)
---
+++ test_bst.py
@@ -0,0 +1,76 @@
+from bst import BinarySearchTree
+# from bst import BSTNode
+import pytest
+
+
+@pytest.fixture(scope="function")
+def empty_bst():
+    return BinarySearchTree()
+
+
+@pytest.fixture(scope="function")
+def optimal_bst():
+    return BinarySearchTree([6, 4, 8, 2, 5, 7, 9])
+
+
+@pytest.fixture(scope="function")
+def degenerate_bst():
+    return BinarySearchTree([6, 4, 2])
+
+
+def test_bst_init():
+    bstree = BinarySearchTree()
+    assert bstree is not None
+    assert bstree.size() == 0
+
+
+def test_bst_init_size(empty_bst):
+    assert empty_bst.size() == 0
+
+
+def test_insert():
+    bst = BinarySearchTree([6, 4, 8, 6])
+    assert bst.root.value == 6
+    assert bst.root.left.value == 4
+    assert bst.root.right.value == 8
+    assert bst.size() == 3
+
+
+def test_contains_empty_BST(empty_bst):
+    assert not empty_bst.contains(4)
+
+
+def test_contains_optimal(optimal_bst):
+    contents = [6, 4, 8, 2, 5, 7, 9]
+    assert [optimal_bst.contains(item) for item in contents]
+    assert not optimal_bst.contains(20)
+
+
+def test_depth(empty_bst):
+    assert empty_bst.depth() == 0
+
+
+def test_depth_optimal(optimal_bst):
+    assert optimal_bst.depth() == 3
+
+
+def test_depth_degenerate(degenerate_bst):
+    assert degenerate_bst.depth() == 3
+
+
+def test_balance_empty(empty_bst):
+    assert empty_bst.balance() == 0
+
+
+def test_balance_optimal(optimal_bst):
+    assert optimal_bst.balance() == 0
+
+
+def test_balance_left_heavy(degenerate_bst):
+    assert degenerate_bst.balance() == 2
+
+
+def test_balance_right_heavy():
+    contents = [6, 8, 10]
+    bst = BinarySearchTree(contents)
+    assert bst.balance() == -2
