50ef485e3b547e109a0976afa473a5085f4ab3ad
Perry Hook <perry.hook@gmail.com>
2014-07-13 17:27:45 -0700

    set and get methods implemented, pass basic tests

--- hash_table.py
+++ hash_table.py
@@ -12,3 +12,20 @@ class HashTable(object):
         for letter in key:
             sum_ord += ord(letter)
         return sum_ord % self._list_size
+
+    def set(self, key, value):
+        hash = self._hash(key)
+        for item in self._bucket_list[hash]:
+            if item[0] == key:
+                item[1] == value
+                return
+        self._bucket_list[hash].append((key, value))
+
+    def get(self, key):
+        hash = self._hash(key)
+        for item in self_bucket_list[hash]:
+            if item[0] == key:
+                return item[1]
+
+
+
--- test_hash_table.py
+++ test_hash_table.py
@@ -35,8 +35,29 @@ def test_hash(empty_table):
         empty_table._hash(9.)
 
 
-def test_set(empty_table, word_list):
-    empty_table.set('first', 1)
-    empty_table.set('second', 2)
-    empty_table.set('third', 3)
+def test_set(empty_table):
+    empty_table.set('test', 1)
+    empty_table.set('sett', 2)
+    empty_table.set('a', 3)
+    empty_table.set('A', 4)
+    empty_table.set('Hello World!', 5)
+    assert empty_table._bucket_list[448][0] == ('test', 1)
+    assert empty_table._bucket_list[448][1] == ('sett', 2)
+    assert empty_table._bucket_list[97][0] == ('a', 3)
+    assert empty_table._bucket_list[65][0] == ('A', 4)
+    assert empty_table._bucket_list[61][0] == ('Hello World!', 5)
+    with pytest.raises(TypeError):
+        empty_table.set(9, 9)
 
+
+def test_get(empty_table):
+    empty_table.set('test', 1)
+    empty_table.set('sett', 2)
+    empty_table.set('a', 3)
+    empty_table.set('A', 4)
+    empty_table.set('Hello World!', 5)
+    assert empty_table.get('test') == 1
+    assert empty_table.get('sett') == 2
+    assert empty_table.get('a') == 3
+    assert empty_table.get('A') == 4
+    assert empty_table.get('Hello World!') == 5
